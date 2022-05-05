a5774fafe887ff4d67eccceda656871880ab6063
Kapil Choudhari <kapil.choudhari@ses.com>
2022-05-05 15:56:28 +0530

    Config CI/CD

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Kapil's Portfolio</title>
+        <title>Kapil&apos;s Portfolio</title>
         <meta name="description" content="Kapil's portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
