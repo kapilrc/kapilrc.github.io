6ee3c6cf94a73652eaba27f775210f265c374f2c
kapilrc <kapilrc@gmail.com>
2022-05-27 06:30:04 +0000

    Deploying to master from  @ 2b42048ca579eae048cb0d803d16139dfbab1712 🚀

--- constants/work.js
+++
@@ -1,9 +0,0 @@
-export const WORK = [
-  {
-    title: "SES",
-    description:
-      "SES is world’s leading content connectivity provider. \nThe Current project is about to - build unified customer portal - a connectivity solutions to SES mPowered customers",
-    link: "#",
-    extra: "",
-  }
-];
