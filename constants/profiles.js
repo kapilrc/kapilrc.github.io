e30cc53d7ae70e449a3b986e37e709d29184b2f5
kapilrc <kapilrc@gmail.com>
2024-02-03 15:26:55 +0530

    resume format updated

--- constants/profiles.js
+++ constants/profiles.js
@@ -5,18 +5,8 @@ export const PROFILES = [
     link: "https://www.linkedin.com/in/kapilchoudhari/",
   },
   {
-    title: "GitHub",
-    logo: "/Github_logo.jpeg",
-    link: "https://github.com/kapilrc/",
-  },
-  {
-    title: "Stackoverflow",
-    logo: "/Stackoverflow_logo.jpeg",
-    link: "https://stackoverflow.com/users/4461073/kapilrc"
-  },
-  {
     title: "Download Resume",
     logo: "/Resume_logo.jpeg",
-    link: "/KapilChoudhari_React_Node.js_Sr.Lead.docx"
+    link: "/KapilChoudhari_JS_React.pdf"
   }
 ];
